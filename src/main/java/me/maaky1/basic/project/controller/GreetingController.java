b8bde59a8294ea6426fd1acd108a9eafe5704db7
M Agus F <aagusfikry1@gmail.com>
2024-12-06 19:41:11 +0700

    Add new function sayHello with parameter

--- src/main/java/me/maaky1/basic/project/controller/GreetingController.java
+++ src/main/java/me/maaky1/basic/project/controller/GreetingController.java
@@ -1,13 +1,27 @@
 package me.maaky1.basic.project.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/say")
 public class GreetingController {
 
-    @GetMapping("/")
+    /**
+     * Say Hello without parameter
+     */
+    @GetMapping("/hello")
     public String sayHello() {
         return "Hello World!";
+    }
+
+    /**
+     * Say Hello with parameter
+     */
+    @GetMapping("/hello-to")
+    public String sayHelloTo(@RequestParam String name) {
+        return "Hello " + name;
     }
 }
