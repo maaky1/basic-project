a9e91e5d4cca78be383811d474fac23f11f9a424
M Agus F <aagusfikry1@gmail.com>
2024-12-06 18:04:00 +0700

    initial commit

---
+++ src/main/java/me/maaky1/basic/project/BasicProjectApplication.java
@@ -0,0 +1,13 @@
+package me.maaky1.basic.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/me/maaky1/basic/project/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package me.maaky1.basic.project.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World!";
+    }
+}
